8e8b3a49d05c68845635b7118a0fff371e4ddf9d
iguridi <iguridi@uc.cl>
2020-10-12 11:15:29 -0300

    feat: get text from file

--- keyslinger-frontend/src/components/TextInput.vue
+++ keyslinger-frontend/src/components/TextInput.vue
@@ -1,86 +1,96 @@
 <template>
   <div class="textinput">
     <div class="items">
-      <token 
-        v-for="(token, index) in tokens" 
-        :key="index" 
-        :token="token" 
+      <token
+        v-for="(token, index) in tokens"
+        :key="index"
+        :token="token"
         :class="[
           currentIndex === index ? ['cursor', 'cursor--blinking'] : '',
-          token.status ? `token--${token.status}` : ''
-        ]"/>
+          token.status ? `token--${token.status}` : '',
+        ]"
+      />
     </div>
   </div>
-</template>  
+</template>
 
 <script>
-import Token from './Token'
+import Token from "./Token";
 
 export default {
-  name: 'text-input',
+  name: "text-input",
   components: {
-    Token
+    Token,
   },
   data: function() {
     return {
       timer: null,
-    }
+    };
   },
-  created: function () {
-    window.addEventListener('keydown', this.checkCharacter);
+  created: function() {
+    window.addEventListener("keydown", this.checkCharacter);
   },
   computed: {
-    tokens: function () {
-      return this.$store.state.tokens
+    tokens: function() {
+      return this.$store.state.tokens;
     },
-    active: function () {
-      return this.$store.state.active
+    active: function() {
+      return this.$store.state.active;
     },
-    completed: function () {
-      return this.$store.state.completed
+    completed: function() {
+      return this.$store.state.completed;
     },
-    currentIndex: function () {
-      return this.$store.state.currentIndex
-    }
+    currentIndex: function() {
+      return this.$store.state.currentIndex;
+    },
   },
   methods: {
-    checkCharacter: function (event) {
-      let key = event.key
+    checkCharacter: function(event) {
+      let key = event.key;
       if (!this.active && key === this.tokens[0].key && !this.completed) {
-        this.$store.dispatch('activate')
+        this.$store.dispatch("activate");
         this.timer = setInterval(() => {
-          this.$store.dispatch('addSecond')
-        }, 1000)
+          this.$store.dispatch("addSecond");
+        }, 1000);
       }
 
       if (this.active && !this.completed) {
-        if (key === 'Backspace') {
-          this.$store.dispatch('decrementIndex')
-          this.$store.dispatch('decrementTyped')
-          this.$store.dispatch('setTokenStatus', { index: this.currentIndex, status: '' })
+        if (key === "Backspace") {
+          this.$store.dispatch("decrementIndex");
+          this.$store.dispatch("decrementTyped");
+          this.$store.dispatch("setTokenStatus", {
+            index: this.currentIndex,
+            status: "",
+          });
         }
         // TODO: check special keys (enter, esq, shift, option)
         else if (key === this.tokens[this.currentIndex].key) {
-          this.$store.dispatch('setTokenStatus', { index: this.currentIndex, status: 'ok' })
-          this.$store.dispatch('incrementIndex')
+          this.$store.dispatch("setTokenStatus", {
+            index: this.currentIndex,
+            status: "ok",
+          });
+          this.$store.dispatch("incrementIndex");
         } else {
-          this.$store.dispatch('setTokenStatus', { index: this.currentIndex, status: 'error' })
-          this.$store.dispatch('incrementErrors')
-          this.$store.dispatch('incrementIndex')
+          this.$store.dispatch("setTokenStatus", {
+            index: this.currentIndex,
+            status: "error",
+          });
+          this.$store.dispatch("incrementErrors");
+          this.$store.dispatch("incrementIndex");
         }
-        this.$store.dispatch('incrementTyped')
+        this.$store.dispatch("incrementTyped");
       }
       if (this.currentIndex === this.tokens.length && !this.completed) {
-        this.$store.dispatch('deactivate')
-        this.$store.dispatch('complete')
-        clearInterval(this.timer)
-        console.log('Done!') // TODO: show a message box
+        this.$store.dispatch("deactivate");
+        this.$store.dispatch("complete");
+        clearInterval(this.timer);
+        alert("Done!");
       }
     },
   },
-  beforeUnmount: function () {
-    clearInterval(this.timer) // in case of emergency?
-  }
+  beforeUnmount: function() {
+    clearInterval(this.timer); // in case of emergency?
+  },
 };
 </script>
 
@@ -98,6 +108,4 @@ export default {
   border-top: 1px solid grey;
   border-bottom: 1px solid grey;
 }
-
-
 </style>
