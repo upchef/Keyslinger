6770d6f052ab4d290ef590b1c31277ce50c8e0c5
iguridi <iguridi@uc.cl>
2020-10-12 11:17:57 -0300

    feat: get text from file

--- keyslinger-frontend/src/store/index.js
+++ keyslinger-frontend/src/store/index.js
@@ -8,155 +8,88 @@ let state = {
   amountTyped: 0,
   totalErrors: 0,
   tokens: [
-    {
-      char: 'c',
-      key: 'c',
-      status: null
-    },
-    {
-      char: 'v',
-      key: 'v',
-      status: null
-    },
-    {
-      char: 'i',
-      key: 'i',
-      status: null
-    },
-    {
-      char: 'o',
-      key: 'o',
-      status: null
-    },
-    {
-      char: 'n',
-      key: 'n',
-      status: null
-    },
-    {
-      char: '␣',
-      key: ' ',
-      status: null
-    },
-    {
-      char: 'p',
-      key: 'p',
-      status: null
-    },
-    {
-      char: 'r',
-      key: 'r',
-      status: null
-    },
-    {
-      char: 'e',
-      key: 'e',
-      status: null
-    },
-    {
-      char: 't',
-      key: 't',
-      status: null
-    },
-    {
-      char: 'y',
-      key: 'y',
-      status: null
-    },
-    {
-      char: '␣',
-      key: ' ',
-      status: null
-    },
-    {
-      char: 'o',
-      key: 'o',
-      status: null
-    },
-    {
-      char: 'r',
-      key: 'r',
-      status: null
-    },
     // {
     //   char: '↩',
     //   key: 'r',
     //   status: null
     // },
-  ]
-}
+  ],
+};
 
 let mutations = {
-  incrementIndex: function (state) {
-    state.currentIndex++
+  setTokens: function(state, string) {
+    state.tokens = [...string].map((s) => ({ char: s, key: s, status: null }));
   },
-  decrementIndex: function (state) {
+  incrementIndex: function(state) {
+    state.currentIndex++;
+  },
+  decrementIndex: function(state) {
     state.currentIndex = Math.max(0, state.currentIndex - 1);
   },
-  setTokenStatus: function (state, { index, status }) {
-    state.tokens[index].status = status
+  setTokenStatus: function(state, { index, status }) {
+    state.tokens[index].status = status;
   },
-  activate: function (state) {
-    state.active = true
+  activate: function(state) {
+    state.active = true;
   },
-  deactivate: function (state) {
-    state.active = false
+  deactivate: function(state) {
+    state.active = false;
   },
-  complete: function (state) {
-    state.completed = true
+  complete: function(state) {
+    state.completed = true;
   },
-  incrementTyped: function (state) {
-    state.amountTyped++
+  incrementTyped: function(state) {
+    state.amountTyped++;
   },
-  decrementTyped: function (state) {
-    state.amountTyped--
+  decrementTyped: function(state) {
+    state.amountTyped--;
   },
-  incrementErrors: function (state) {
-    state.totalErrors++
+  incrementErrors: function(state) {
+    state.totalErrors++;
   },
-  addSecond: function (state) {
-    state.seconds++
+  addSecond: function(state) {
+    state.seconds++;
   },
-  subtractSecond: function (state) {
-    state.seconds--
+  subtractSecond: function(state) {
+    state.seconds--;
   },
-}
+};
 
 let actions = {
-  incrementIndex: function (context) {
-    context.commit('incrementIndex')
+  incrementIndex: function(context) {
+    context.commit("incrementIndex");
   },
-  decrementIndex: function (context) {
-    context.commit('decrementIndex')
+  decrementIndex: function(context) {
+    context.commit("decrementIndex");
   },
-  setTokenStatus: function (context, { index, status }) {
-    context.commit('setTokenStatus', { index, status })
+  setTokenStatus: function(context, { index, status }) {
+    context.commit("setTokenStatus", { index, status });
   },
-  activate: function (context) {
-    context.commit('activate')
+  activate: function(context) {
+    context.commit("activate");
   },
-  deactivate: function (context) {
-    context.commit('deactivate')
+  deactivate: function(context) {
+    context.commit("deactivate");
   },
-  complete: function (context) {
-    context.commit('complete')
+  complete: function(context) {
+    context.commit("complete");
   },
-  incrementTyped: function (context) {
-    context.commit('incrementTyped')
+  incrementTyped: function(context) {
+    context.commit("incrementTyped");
   },
-  decrementTyped: function (context) {
-    context.commit('decrementTyped')
+  decrementTyped: function(context) {
+    context.commit("decrementTyped");
   },
-  incrementErrors: function (context) {
-    context.commit('incrementErrors')
+  incrementErrors: function(context) {
+    context.commit("incrementErrors");
   },
-  addSecond: function (context) {
-    context.commit('addSecond')
+  addSecond: function(context) {
+    context.commit("addSecond");
   },
-  subtractSecond: function (context) {
-    context.commit('subtractSecond')
-  }
-}
+  subtractSecond: function(context) {
+    context.commit("subtractSecond");
+  },
+};
 
 // let getters = {}
 
